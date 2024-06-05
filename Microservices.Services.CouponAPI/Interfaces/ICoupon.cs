991990de47b04573f059494cc1ce7146d562d3ae
JadAlKhawand <alkhawandjad@gmail.com>
2024-06-05 16:13:18 +0300

    Created CRUD apis

---
+++ Microservices.Services.CouponAPI/Interfaces/ICoupon.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservices.Services.CouponAPI.Interfaces
+{
+    public class ICoupon
+    {
+        Guid CouponId { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        double Discount { get; set; }
+
+
+    }
+}
