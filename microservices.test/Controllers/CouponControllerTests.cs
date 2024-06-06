920c90c2329c0ad0046c39ad5175d79f4b49d532
JadAlKhawand <alkhawandjad@gmail.com>
2024-06-06 13:33:01 +0300

    added unit testing for getAllCoupons API

--- microservices.test/Controllers/CouponControllerTests.cs
+++ microservices.test/Controllers/CouponControllerTests.cs
@@ -6,6 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FakeItEasy;
+using AutoMapper;
+using Microservices.Services.CouponAPI.Data;
+using Microservices.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using FakeItEasy.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace microservices.test.Controllers
 {
@@ -15,8 +22,30 @@ namespace microservices.test.Controllers
         public void CouponController_GetAllCoupons_ReturnsAllCoupons()
         {
             // Arrange
-            var dataStore = A.Fake<ICoupon>();
-            var controller = new CouponAPIController();  
+
+            var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "FakeDb")
+            .Options;
+
+            var fakeDb = new AppDbContext(dbContextOptions);
+            var fakeMapper = A.Fake<IMapper>();
+            
+            var fakeCoupons = A.CollectionOfFake<Coupon>(10);
+            var fakeCouponsList = A.Fake<List<Coupon>>();
+
+            A.CallTo(() => fakeMapper.Map<List<Coupon>>(fakeCoupons)).Returns(fakeCouponsList);
+
+            var controller = new CouponAPIController(fakeDb, fakeMapper);
+
+            // Act
+
+            var results = controller.GetAllCoupons();
+
+            // Assert
+
+            Assert.NotNull(results);
+            Assert.IsType<OkObjectResult>(results);
+
         }
     }
 }
